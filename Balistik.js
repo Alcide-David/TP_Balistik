2007e47d9ed75e8dd04c3c63281856981c7ab9b2
Alcide-David <152320912+Alcide-David@users.noreply.github.com>
2023-12-07 20:47:17 -0500

    Update Balistik.js

--- Balistik.js
+++ Balistik.js
@@ -1,16 +1,20 @@
 const balistik = {
   _alph: 'abcdefghijklmnopqrstuvwxyz',
   _salt: "Ux00",
+
   encrypt: function (str) {
     let encrypted = "";
     for (let i = 0; i < str.length; i++) {
       let char = str[i];
+      //Condition nous permettant de savoir si str contient que des caracteres de _alph
       if (!this._alph.includes(char.toLowerCase())) {
-        throw new ValueError("Invalid character found: " + char);
+        throw new ValueError(" | ERREUR | -> Ce caractere ne correspond pas : " + char);
       }
 
       let index = this._alph.indexOf(char.toLowerCase());
+      //Verification si l'existe un caractere en Majuscule
       if (isUpper(char)) {
+        //Si oui, on ajoute un 0 a l'indice de ce caractere
         index = "0" + String(index);
       } else {
         index = String(index);
@@ -21,21 +25,25 @@ const balistik = {
     encrypted = this._salt + "." + encrypted + str.length;
     return encrypted;
   },
+
+
   decrypt: function (str) {
     let parts = str.split(".");
+    //Verification si le cle contient l'indice _salt
     if (parts[0] !== this._salt) {
-      throw new ValueError("Incorrect salt");
+      throw new ValueError(" | ERREUR | -> Le _salt ne correspond pas ");
     }
 
+    //Sachant que la longueur du mot est a la fin, on verifie cette longueur avant de passer au decryptage
     if (parts.length - 2 !== parseInt(parts[parts.length - 1])) {
-      throw new ValueError("Invalid length");
+      throw new ValueError(" | ERREUR | -> La longueur de ce cle est incorrecte");
     }
 
     let decrypted = "";
-    for (let i = 2; i < parts.length - 1; i++) {
+    //Boucle permettant le decryptage
+    for (let i = 1; i < parts.length - 1; i++) {
       let charIndex = parseInt(parts[i]);
       if (parts[i].startsWith("0")) {
-        charIndex = charIndex - 1;
         decrypted += this._alph[charIndex].toUpperCase();
       } else {
         decrypted += this._alph[charIndex];
@@ -45,22 +53,20 @@ const balistik = {
   },
 };
 
-// Function to check if a character is lowercase
+// Fonction pour savoir si un caractere est en Minuscule
 function isLower(letter) {
   return letter.toLowerCase() === letter;
 }
 
-// Function to check if a character is uppercase
+// Fonction pour savoir si un caractere est en Majuscule
 function isUpper(letter) {
   return letter.toUpperCase() === letter;
 }
 
-// Custom error classes
-class ValueError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "ValueError";
-  }
+//Notre fonction ValueError
+function ValueError(message) {
+  const error = new Error(message);
+  return error;
 }
 
 // Test
